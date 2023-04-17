259df06c2d95f57315ee9ce4ca658c29c6f3d57d
Daniel St <daniel.steinmaurer@htl-braunau.at>
2023-04-17 20:36:51 +0200

    add more complex tests

--- sample/shopping_cart.py
+++ sample/shopping_cart.py
@@ -1,2 +1,26 @@
+from typing import Self, List
+from dataclasses import dataclass
+
+@dataclass
+class ShoppingItem:
+    """Represents a Shopping Cart Item"""
+    name: str
+    price:int 
+
 class ShoppingCart:
-    ...
+    def __init__(self):
+        self._items:List[ShoppingItem] = []
+
+    @property
+    def items(self) -> List[ShoppingItem]:
+        return self.items
+    
+    def append(self, item:ShoppingItem):
+        self._items.append(item)
+    
+    def __len__(self):
+        return len(self._items)
+    
+    def __getitem__(self, i:int)->ShoppingItem:
+        return self._items[i]
+    
--- test/test_shopping_cart.py
+++ test/test_shopping_cart.py
@@ -1,6 +1,7 @@
 import sample
 import sample.shopping_cart
 from sample.shopping_cart import ShoppingCart
+from sample.shopping_cart import ShoppingItem
 
 import unittest
 
@@ -10,6 +11,12 @@ def test_creation():
     """
     cart = ShoppingCart()
 
+def test_append():
+    cart = ShoppingCart()
+    item = ShoppingItem('tom', 1)
+    cart.append(item)
+
+    assert len(cart._items) == 1 and cart[0] is item
 
 class TestShoppingCart(unittest.TestCase):
 
@@ -18,3 +25,17 @@ class TestShoppingCart(unittest.TestCase):
             Tests if the Creation via Default Constructor throws an Exception
         """
         cart = ShoppingCart()
+
+    def test_append(self):
+        cart = ShoppingCart()
+        item = ShoppingItem('tom', 1)
+        cart.append(item)
+
+        self.assertEqual(len(cart._items), 1)
+        self.assertTrue(cart[0] is item)
+    
+    def test_exception(self):
+        with self.assertRaises(Exception):
+            raise Exception('The Context Manager expects this Exception')
+        
+        #Exception('This Exception will cause the Test to fail')
