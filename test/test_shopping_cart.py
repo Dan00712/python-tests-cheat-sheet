544413d33e4945d408be67d3ddc23a07dfb62bbf
Daniel St <daniel.steinmaurer@htl-braunau.at>
2023-10-09 14:57:17 +0200

    reformat test_shopping_cart.py with black

--- test/test_shopping_cart.py
+++ test/test_shopping_cart.py
@@ -5,37 +5,39 @@ from sample.shopping_cart import ShoppingItem
 
 import unittest
 
+
 def test_creation():
     """
-        Tests if the Creation via Default Constructor throws an Exception
+    Tests if the Creation via Default Constructor throws an Exception
     """
     cart = ShoppingCart()
 
+
 def test_append():
     cart = ShoppingCart()
-    item = ShoppingItem('tom', 1)
+    item = ShoppingItem("tom", 1)
     cart.append(item)
 
     assert len(cart._items) == 1 and cart[0] is item
 
-class TestShoppingCart(unittest.TestCase):
 
+class TestShoppingCart(unittest.TestCase):
     def test_creation(self):
         """
-            Tests if the Creation via Default Constructor throws an Exception
+        Tests if the Creation via Default Constructor throws an Exception
         """
         cart = ShoppingCart()
 
     def test_append(self):
         cart = ShoppingCart()
-        item = ShoppingItem('tom', 1)
+        item = ShoppingItem("tom", 1)
         cart.append(item)
 
         self.assertEqual(len(cart._items), 1)
         self.assertTrue(cart[0] is item)
-    
+
     def test_exception(self):
         with self.assertRaises(Exception):
-            raise Exception('The Context Manager expects this Exception')
-        
-        #Exception('This Exception will cause the Test to fail')
+            raise Exception("The Context Manager expects this Exception")
+
+        # Exception('This Exception will cause the Test to fail')
